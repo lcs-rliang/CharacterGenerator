5861db39e7666a122c2f7a97575a5a648b419df8
Ruiyan Liang <Jliang@lcs.on.ca>
2023-06-01 14:20:42 -0400

    Initial Commit

---
+++ CharacterGenerator/CharacterGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterGeneratorApp.swift
+//  CharacterGenerator
+//
+//  Created by dyhidrogen monoxide on 2023/6/1.
+//
+
+import SwiftUI
+
+@main
+struct CharacterGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
