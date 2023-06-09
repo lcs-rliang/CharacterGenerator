6ccb7e398641864a95de0876bfdc04b5a4f9795b
Ruiyan Liang <Jliang@lcs.on.ca>
2023-06-09 12:46:23 -0400

    Used the AppDatabase helper code to establish the database connection and make it available within the app.

--- CharacterGenerator/CharacterGeneratorApp.swift
+++ CharacterGenerator/CharacterGeneratorApp.swift
@@ -4,14 +4,23 @@
 //
 //  Created by dyhidrogen monoxide on 2023/6/1.
 //
-
+import Blackbird
 import SwiftUI
 
 @main
 struct CharacterGeneratorApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            GeneratorView(player: "Harvey Walter",
+                          name: "Chaosium",
+                          occupation: "Writer",
+                          era: "1920s",
+                          age: "42",
+                          sex: "Male",
+                          Residence: "New Jersey",
+                          Birthplace: "Louisiana")
+            
+                .environment(\.blackbirdDatabase, AppDatabase.instance)
         }
     }
 }
